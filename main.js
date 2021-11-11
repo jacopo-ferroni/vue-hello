f355e432ebcac88a8f0fc557efe4b18f8f768221
jacopo-ferroni <jacopoferroni2706@gmail.com>
2021-11-11 13:22:14 +0100

    end

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log(Vue);
+
+// inizzializzazione Vue
+const app = new Vue({
+    el : `#app`,
+    data : {
+        text : `Hello World`,
+        img : `https://picsum.photos/id/237/200/300`,
+    }
+});
